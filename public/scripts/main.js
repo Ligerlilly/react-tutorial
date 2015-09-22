33ed11958d096d794512c7046e7e3173366743b4
Jason <quizathaderat@yahoo.com>
2015-09-21 21:35:30 -0700

    Add new comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,13 +15,27 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+  handleCommentSubmit: function(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data :data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
 
   render: function() {
     return (
       <div className='commentBox'>
         <h1>Comment</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
@@ -43,11 +57,28 @@ var CommentList = React.createClass({
   }
 });
 var CommentForm = React.createClass({
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = React.findDOMNode(this.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author ) {
+     return;
+    }
+
+    this.props.onCommentSubmit({ author: author, text: text });
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+    return;
+  },
   render: function() {
     return (
-      <div className='commentForm'>
-        Hello world! I am a CommentForm.
-      </div>
+      <form className='commentForm' onSubmit={this.handleSubmit}>
+        <input type='text' placeholder='Your name' ref='author'/>
+        <input type='text' placeholder='Say somthing...' ref='text' />
+        <input type='submit' value='Post' />
+
+      </form>
     );
   }
 });
